0b823ecf06404fd882c6f388e7a27b7e073fe980
JChow320 <jasonchow9168@yahoo.com.hk>
2021-04-09 20:19:01 -0400

    first push

---
+++ Lesson_1/app.js
@@ -0,0 +1,6 @@
+$('#light').click(function(){
+	$('body').css('background-color', "powderblue");
+	$('.boxes').css('background-color', "cornsilk");
+	$('.content').css('background-color', "PeachPuff");
+	$('body').css('color', "navy");
+});
